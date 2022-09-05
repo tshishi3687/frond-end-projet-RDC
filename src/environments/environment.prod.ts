9575c432a1f26101279b39335fbbae16c233e7a1
tshishi3687 <tshishi@outlook.be>
2022-09-05 23:04:41 +0200

    test deployement 6

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   // serveur_url: 'http://10.10.19.45:8081'
   // serveur_url: 'http://mobembo.hopto.org:8081'
-  serveur_url: 'http://mobembo.online:8081'
-  // serveur_url: 'http://localhost:8081'
+  // serveur_url: 'http://mobembo.online:8081'
+  serveur_url: 'http://localhost:8081'
   // serveur_url: 'http://10.2.12.158:8081'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -7,8 +7,8 @@ export const environment = {
   // serveur_url: 'http://10.10.19.45:8081'
   // serveur_url: 'http://mobembo.hopto.org:8081'
   // serveur_url: 'http://127.0.0.1:8081'
-  serveur_url: 'http://mobembo.online:8081'
-  // serveur_url: 'http://10.2.12.158:8081'
+  // serveur_url: 'http://mobembo.online:8081'
+  serveur_url: 'http://localhost:8081'
 };
 
 /*
